de510c17c48dd368001310a82de87915d97917ad
Onoshchenko Alyona <eco.makarova.mgb1@mail.ru>
2025-12-16 11:32:34 +0500

    12

--- Potok3.cpp
+++ Potok3.cpp
@@ -5,50 +5,61 @@
 #include <thread>
 #include <chrono>
 #include <mutex>
+#include <random>
 #include <iomanip>
 
-
-const int NUM_THREADS = 4;          
-const int BAR_WIDTH = 40;   
-const int STEP_MS = 100;  
+const int NUM_THREADS = 4;
+const int BAR_WIDTH = 40;
+const int MIN_STEP_MS = 50;
+const int MAX_STEP_MS = 200;
 
 std::mutex print_mutex;
+
 struct ThreadInfo {
-	int idx;                   
-	std::thread::id tid;       
-	double exec_time = 0.0;   
+	int idx;
+	double exec_time = 0.0;
 	bool done = false;
 };
 
 std::vector<ThreadInfo> threads(NUM_THREADS);
+
+
+int get_random_delay() {
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(MIN_STEP_MS, MAX_STEP_MS);
+	return dis(gen);
+}
+
+
 void update_progress(int thread_idx, int progress) {
 	std::lock_guard<std::mutex> lock(print_mutex);
-	std::cout << "\033[" << (thread_idx + 2) << "A";
+	std::cout << "\033[" << (thread_idx + 2) << ";1H";
+	std::cout << std::string(80, ' ') << "\r";
 	std::string bar(BAR_WIDTH, ' ');
 	for (int i = 0; i < progress; ++i) {
-		bar[i] = '█';
+		bar[i] = '#';
 	}
 
 	int percent = (progress * 100) / BAR_WIDTH;
-	std::cout << std::setw(2) << thread_idx << " | "
-		<< threads[thread_idx].tid << " | ["
+	std::cout << std::setw(2) << thread_idx << " | ["
 		<< bar << "] | " << percent << "%";
 	if (threads[thread_idx].done) {
 		std::cout << " | " << std::fixed << std::setprecision(3)
 			<< threads[thread_idx].exec_time << " сек";
 	}
-
 	std::cout << std::endl;
 	std::cout.flush();
 }
+
+
 void thread_func(int thread_idx) {
 	auto start = std::chrono::high_resolution_clock::now();
 	threads[thread_idx].idx = thread_idx;
-	threads[thread_idx].tid = std::this_thread::get_id();
-
 	update_progress(thread_idx, 0);
 	for (int step = 1; step <= BAR_WIDTH; ++step) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(STEP_MS));
+		int delay_ms = get_random_delay();
+		std::this_thread::sleep_for(std::chrono::milliseconds(delay_ms));
 		update_progress(thread_idx, step);
 	}
 	auto end = std::chrono::high_resolution_clock::now();
@@ -60,11 +71,8 @@ void thread_func(int thread_idx) {
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	for (int i = 0; i < NUM_THREADS; ++i) {
-		std::cout << std::setw(2) << i << " | "
-			<< "             " << " | ["
-			<< std::string(BAR_WIDTH, ' ') << "] | 0%\n";
-	}
+	std::cout << "Поток | Прогресс                           | %   | Время\n";
+	std::cout << std::string(60, '-') << "\n";
 	std::vector<std::thread> thread_pool;
 	for (int i = 0; i < NUM_THREADS; ++i) {
 		thread_pool.emplace_back(thread_func, i);
@@ -72,6 +80,7 @@ int main() {
 	for (auto& t : thread_pool) {
 		t.join();
 	}
-	std::cout << std::string(70, '-') << "\n";
+	std::cout << std::string(60, '-') << "\n";
+
 	return 0;
 }
